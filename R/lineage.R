6875b6e8d58ca969ab21b489a259b97133d8006b
Josh Wills <jwills@cloudera.com>
2013-12-16 08:09:51 -0800

    Support for factors and other data types

--- R/lineage.R
+++ R/lineage.R
@@ -17,34 +17,41 @@ Lineage <- function(existing=list()) {
 
 ### LineageVector declarations
 
-setClass("LineageVector",
+setClass("AbstractLineage",
          representation(id="numeric", parents="numeric", lineage="function",
-                        fn="character", args="list"),
-         contains="vector")
+                        fn="character", args="list"))
+setClass("LineageVector",
+         contains=c("vector", "AbstractLineage"))
+setClass("LineageFactor", contains=c("factor", "AbstractLineage"))
 
 unit.lv <- function(vec, fn="constant", args=list(),
                     parents=numeric(0), lineage=NULL) {
-  lv <- new("LineageVector", vec, parents=parents, 
-            fn=fn, args=args,
-            lineage=lineage)
-  return(lineage(lv))
+  cls <- ifelse(inherits(vec, "factor"), "LineageFactor", "LineageVector")
+  lv <- new(cls, vec, parents=parents, fn=fn, args=args, lineage=lineage)
+  lineage(lv)
 }
+
 map.lv <- function(fn, vecs, args=list()) {
   lins <- unique(
     lapply(
-      Filter(function(v) { inherits(v, "LineageVector") }, vecs),
+      Filter(function(v) { inherits(v, "AbstractLineage") }, vecs),
       function(v) { v@lineage }))
   if (length(lins) != 1) {
     stop("Cannot reconcile parent lineages in map.lv")
   }
   lvecs <- lapply(vecs, function(v) {
-    if (inherits(v, "LineageVector")) {
+    if (inherits(v, "AbstractLineage")) {
       return(v)
     } else {
       return(unit.lv(v, lineage=lins[[1]]))
     }
   })
   parents <- sapply(lvecs, function(v) { v@id })
+  if (inherits(parents, "list")) {
+    print(parents)
+    print(fn)
+    print(vecs)
+  }
   vargs <- lapply(lvecs, as.vector)
   value <- do.call(fn, c(vargs, args))
   unit.lv(value, fn=fn, args=args, parents=parents, lineage=lins[[1]])
@@ -52,21 +59,33 @@ map.lv <- function(fn, vecs, args=list()) {
 
 # Override the core method for displaying LineageVectors so that
 # they usually look like regular vectors
-setMethod("show", signature("LineageVector"),
-          function(object) { show(as.vector(object)) })
-setMethod("names", signature("LineageVector"),
+setMethod("show", signature("AbstractLineage"),
+          function(object) {
+            cls <- ifelse(inherits(object, "LineageFactor"), "factor", "vector")
+            show(as(object, cls))
+          })
+setMethod("print", signature("AbstractLineage"),
+          function(x) {
+            if(inherits(x, "LineageFactor")) {
+              print(factor(as.character(x))) 
+            } else {
+              print(as.vector(x))
+            }
+          })
+setMethod("names", signature("AbstractLineage"),
           function(x) { attr(x, "names") })
-setMethod("names<-", signature("LineageVector", "vector"),
+setMethod("names<-", signature("AbstractLineage", "vector"),
           function(x, value) { attr(x, "names") <- as.character(value); x })
 
 # Pattern for single-arg primitive R functions on LineageVectors
-xfunc <- function(name) {
-  setMethod(name, signature("LineageVector"),
-            function(x) { map.lv(name, list(x))})
+xfunc <- function(name, cls="LineageVector") {
+  setMethod(name, signature(cls), function(x) { map.lv(name, list(x))})
 }
 sapply(c("abs", "log", "log10", "sqrt", "floor", "ceiling",
-         "is.na", "!",
-         "toupper", "tolower"), xfunc)
+         "is.na", "!", "tolower", "toupper"), xfunc)
+sapply(c("as.character", "as.numeric", "as.double", "as.factor"), xfunc)
+sapply(c("tolower", "toupper", "as.integer", "as.character"),
+       xfunc, cls="LineageFactor")
 
 # Pattern for binary-arg primitive R functions on LineageVectors
 e1e2func <- function(name) {
@@ -93,20 +112,20 @@ check.lineage <- function(ldf, v) {
   if (is.null(v)) {
     return()
   }
-  if (!inherits(v, "LineageVector")) {
-    stop("Only LineageVectors may be assigned to LineageDataFrames")
+  if (!inherits(v, "AbstractLineage")) {
+    stop("Only lineage vectors may be assigned to lineage data.frames")
   }
   if (length(ldf) > 0) {
     lin <- ldf[[1]]@lineage
     if (!identical(lin, v@lineage)) {
-      stop("Incompatible lineages in LineageDataFrame assignment")
+      stop("Incompatible lineages in lineage data.frame assignment")
     }
     if (!identical(lin()[[v@id]], v)) {
       vo <- lin()[[v@id]]
       if (all(as.vector(vo) == as.vector(v))) {
         lin(v, v@id) # Perform an update
       } else {
-        stop("Input vector was created with unsupported lineage function")
+        stop("Input value was created with unsupported lineage function")
       }
     }
   }
